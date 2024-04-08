2a655aceab3798e7d4e810ec0cb7a9bc96f32dd0
a7med3365 <a7med3365@gmail.com>
2024-04-09 00:14:08 +0300

    Add export handler

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", handlers.MainHandler)
 	http.HandleFunc("/ascii-art", handlers.AsciiHandler)
+	http.HandleFunc("/export", handlers.ExportHandler)
 	fmt.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
