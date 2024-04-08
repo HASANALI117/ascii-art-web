9075960a33a11d32913c2282b8250976d3d202a9
a7med3365 <a7med3365@gmail.com>
2024-04-09 00:13:36 +0300

    extracted common functionality into a function

--- handlers/asciiHandler.go
+++ handlers/asciiHandler.go
@@ -2,7 +2,8 @@ package handlers
 
 import (
 	asciiArt "asciiArt/asciiArt"
-	errors "asciiArt/errors"
+	myErrors "asciiArt/errors"
+	"errors"
 	"net/http"
 	"strings"
 )
@@ -10,50 +11,54 @@ import (
 func AsciiHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		str := r.FormValue("input")
-		font := r.FormValue("font")
-
-		switch strings.ToLower(font) {
-		case "standard", "shadow", "thinkertoy":
-			// font is valid, do nothing
-		default:
-			// http.Error(w, "Invalid font option", http.StatusBadRequest)
+		art, err1 := getAsciiArtFromRequest(r)
+		if err1 != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			templates.ExecuteTemplate(w, "error.html", errors.BadRequest("Invalid font option"))
+			templates.ExecuteTemplate(w, "error.html", myErrors.BadRequest(err1.Error()))
 			return
 		}
-
-		lines := strings.Split(str, "\r\n")
-
-		var asciiLines []string
-		for _, line := range lines {
-			if line == "" {
-				continue
-			}
-			asciiLine, err := asciiArt.AsciiLine(line, font)
-			if err != nil {
-				// http.Error(w, err.Error(), http.StatusBadRequest)
-				w.WriteHeader(http.StatusBadRequest)
-				templates.ExecuteTemplate(w, "error.html", errors.BadRequest(err.Error()))
-				return
-			}
-			asciiLines = append(asciiLines, asciiLine...)
-		}
-
-		art := asciiArt.PrintAscii(asciiLines)
 
 		err := templates.ExecuteTemplate(w, "result.html", art)
 		if err != nil {
 			// http.Error(w, err.Error(), http.StatusInternalServerError)
 			w.WriteHeader(http.StatusInternalServerError)
-			templates.ExecuteTemplate(w, "error.html", errors.InternalServer(err.Error()))
+			templates.ExecuteTemplate(w, "error.html", myErrors.InternalServer(err.Error()))
 			return
 		}
 
 	default:
 		// http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		templates.ExecuteTemplate(w, "error.html", errors.MethodNotAllowed)
+		templates.ExecuteTemplate(w, "error.html", myErrors.MethodNotAllowed)
 	}
 
+}
+
+func getAsciiArtFromRequest(r *http.Request) (string, error) {
+	str := r.FormValue("input")
+	font := r.FormValue("font")
+
+	switch strings.ToLower(font) {
+	case "standard", "shadow", "thinkertoy":
+		// font is valid, do nothing
+	default:
+		return "", errors.New("invalid font option")
+	}
+
+	lines := strings.Split(str, "\r\n")
+
+	var asciiLines []string
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		asciiLine, err := asciiArt.AsciiLine(line, font)
+		if err != nil {
+			return "", err
+		}
+		asciiLines = append(asciiLines, asciiLine...)
+	}
+
+	art := asciiArt.PrintAscii(asciiLines)
+	return art, nil
 }
