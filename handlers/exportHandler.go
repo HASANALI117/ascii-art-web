7d62499a9b760bfd13632660c68f4e0722f145cd
hadhaif <Hassanali61911@gmail.com>
2024-04-10 02:31:04 +0300

    replace servefile with io.Copy to remove result.txt at the end.

--- handlers/exportHandler.go
+++ handlers/exportHandler.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	errors "asciiArt/errors"
+	"io"
+	"log"
 	"net/http"
 	"os"
 )
@@ -22,7 +24,6 @@ func ExportHandler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			templates.ExecuteTemplate(w, "error.html", errors.InternalServer("failed to create file"))
 		}
-		defer file.Close()
 
 		_, err = file.WriteString(art)
 		if err != nil {
@@ -30,12 +31,38 @@ func ExportHandler(w http.ResponseWriter, r *http.Request) {
 			templates.ExecuteTemplate(w, "error.html", errors.InternalServer("failed to write to file"))
 		}
 
+		file.Close()
+
+		// Open the file for reading
+		file, err = os.Open("result.txt")
+		if err != nil {
+			log.Printf("failed to open file: %v", err)
+			return
+		}
+		defer file.Close()
+
 		// Set the appropriate headers to indicate that it's a file download
 		w.Header().Set("Content-Disposition", "attachment; filename=result.txt")
 		w.Header().Set("Content-Type", "text/plain")
+		// w.Header().Set("Content-Length", string(num))
 
 		// Write the file content to the response
-		http.ServeFile(w, r, "result.txt")
+		// http.ServeFile(w, r, "result.txt")
+		_, err = io.Copy(w, file)
+		if err != nil {
+			log.Printf("failed to write file to response: %v", err)
+			return
+		}
+
+		// Ensure the file is closed before trying to delete it
+		file.Close()
+
+		// Delete the file after it's served
+		err = os.Remove("result.txt")
+		if err != nil {
+			log.Printf("failed to delete file: %v", err)
+		}
+
 	default:
 		// http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		w.WriteHeader(http.StatusMethodNotAllowed)
